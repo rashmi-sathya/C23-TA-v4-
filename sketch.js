fb208b4fea79e5ab9168edc1800e4d4c914a6fe0
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-07-22 10:51:04 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,9 @@
 
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
 
 function setup() {
     createCanvas(400,400);
 
-    engine = Engine.create();
-  world = engine.world;
+   
 }
 
 function draw() 
